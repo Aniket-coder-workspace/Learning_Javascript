58b14b4ddcd2017e0ba8f8eb45988714c8416de1
Aniket <aneecoder9789@gmail.com>
2024-07-07 23:18:12 +0530

    update js file

---
+++ Lec4/01_variable.js
@@ -0,0 +1,20 @@
+//1st person account details
+const accountId = 12345;
+let accountEmail = "anm345@gmail.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+
+//2nd person account details
+const accountId1 = 4568;
+let accountEmail1 = "achd8987@gmail.com";
+var accountPassword1 = "798809fg";
+accountCity1 = "Dehli";
+
+//printing data in tabel form we use console.table();
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId1, accountEmail1, accountPassword1, accountCity1]);
+
+// This is one type of to assign values
+let book;
+book = "Rich Dad Poor Dad";
+console.log(book);
