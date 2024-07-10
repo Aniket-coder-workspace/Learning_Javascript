cb9f689c93f2ef66c6043cae8171f3984612851d
Aniket <aneecoder9789@gmail.com>
2024-07-10 10:58:22 +0530

    new file add

---
+++ Chapter 1/Chapter 1 Practice set/Ch1_Practice.js
@@ -0,0 +1,25 @@
+// Chapter 1 Practice set Q.1 
+let name = "Sam";
+let num = 7;
+console.log(name+num);
+
+// Chapter 1 Practice set Q.2
+console.log(typeof(name+num)); // Here output is String 
+
+// Chapter 1 Practice set Q.3
+const obj = {
+    name: "sakshi",
+    age: 19
+}
+obj['name1'] = "Aniket";
+obj['age1'] = 19;
+console.log(obj); // Output { name: 'sakshi', age: 19, name1: 'Aniket', age1: 19 }
+
+// Chapter 1 Practice set Q.4 -- to create a dictionary
+const dict = {
+    book: "a written work that is published as printed pages fastened together inside a cover",
+    car: "a road vehicle with four wheels that can carry a small number of people",
+    love: "a strong feeling that you have when you like somebody/something very much",
+    ball: "a round object that you hit, kick, throw, etc. in games and sports",
+    bat: "a piece of wood for hitting the ball in sports such as table tennis, cricket or baseball"
+}
